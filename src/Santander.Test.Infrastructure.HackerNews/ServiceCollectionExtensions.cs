417ad5a5b1902b0bcbce4e6256a54383383db071
Artem Deryugo <artemderyugo@coherentsolutions.com>
2025-02-25 19:28:09 +0100

    add resilence to HttpClient

--- src/Santander.Test.Infrastructure.HackerNews/ServiceCollectionExtensions.cs
+++ src/Santander.Test.Infrastructure.HackerNews/ServiceCollectionExtensions.cs
@@ -1,4 +1,6 @@
 ﻿using Microsoft.Extensions.DependencyInjection;
+using Polly;
+using Polly.Extensions.Http;
 using Santander.Test.Application.Services.Abstractions;
 using Santander.Test.Infrastructure.HackerNews.Services;
 
@@ -8,10 +10,25 @@ public static class ServiceCollectionExtensions
 {
     public static IServiceCollection AddHackerNewsProvider(this IServiceCollection services)
     {
-        services.AddHttpClient<IExternalStoryProvider, HackerNewsStoryProvider>(client =>
-        {
-            client.BaseAddress = new Uri("https://hacker-news.firebaseio.com");
-        });
+        var retryPolicy = HttpPolicyExtensions
+            .HandleTransientHttpError()
+            .WaitAndRetryAsync(5, retryAttempt => TimeSpan.FromSeconds(Math.Pow(2, retryAttempt)));
+        var circuitBreakerPolicy = HttpPolicyExtensions
+            .HandleTransientHttpError()
+            .CircuitBreakerAsync(10, TimeSpan.FromSeconds(30));
+        var bulkheadPolicy = Policy
+            .BulkheadAsync<HttpResponseMessage>(10, int.MaxValue);
+        var resilientPolicy = Policy.WrapAsync(
+            retryPolicy,
+            circuitBreakerPolicy,
+            bulkheadPolicy);
+
+        services
+            .AddHttpClient<IExternalStoryProvider, HackerNewsStoryProvider>(client =>
+            {
+                client.BaseAddress = new Uri("https://hacker-news.firebaseio.com");
+            })
+            .AddPolicyHandler(resilientPolicy);
 
         return services;
     }
