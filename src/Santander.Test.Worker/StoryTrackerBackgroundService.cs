4c7a01f96830928114b6e8deb67816e4f43b1667
Artem Deryugo <artemderyugo@coherentsolutions.com>
2025-02-25 20:35:44 +0100

    add some error logging for background job

--- src/Santander.Test.Worker/StoryTrackerBackgroundService.cs
+++ src/Santander.Test.Worker/StoryTrackerBackgroundService.cs
@@ -3,18 +3,29 @@ using Santander.Test.Application.Services.Abstractions;
 
 namespace Santander.Test.Worker;
 
-public class StoryTrackerBackgroundService(IServiceProvider serviceProvider) : BackgroundService
+public class StoryTrackerBackgroundService(
+    IServiceProvider serviceProvider,
+    ILogger<StoryTrackerBackgroundService> logger) : BackgroundService
 {
     private readonly IServiceProvider _serviceProvider = serviceProvider;
+    private readonly ILogger<StoryTrackerBackgroundService> _logger = logger;
 
     protected override async Task ExecuteAsync(CancellationToken stoppingToken)
     {
         while (!stoppingToken.IsCancellationRequested)
         {
-            using var serviceScope = _serviceProvider.CreateScope();
-            var service = serviceScope.ServiceProvider.GetRequiredService<IUpdateStoryService>();
-            await service.UpdateBestStoriesAsync(stoppingToken);
-            await Task.Delay(10000, stoppingToken);
+            try
+            {
+                using var serviceScope = _serviceProvider.CreateScope();
+                var service = serviceScope.ServiceProvider.GetRequiredService<IUpdateStoryService>();
+                await service.UpdateBestStoriesAsync(stoppingToken);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, "Hacker News polling failed with the error: {Message}", ex.Message);
+            }
+
+            await Task.Delay(30 * 1000, stoppingToken);
         }
     }
 }
